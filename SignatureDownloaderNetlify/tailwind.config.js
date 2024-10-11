8795656a58aa981a9cd34797ac7c204418dfd0de
MD SAJID ANWAR <111846893+sajidzayn21@users.noreply.github.com>
2024-10-11 14:50:37 +0530

    Add files via upload

---
+++ SignatureDownloaderNetlify/tailwind.config.js
@@ -0,0 +1,15 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["*"],
+  theme: {
+    extend: {
+      colors: {
+        'custom-black': '#000000',
+        'custom-black-light': '#282828',
+        
+      },
+    },
+  },
+  plugins: [],
+}
+
